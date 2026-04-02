e3f9ca832837193e9c5727b9ba852ce3c2e3416d
Jáchym Purnoch <jachym.purnoch@tul.cz>
2026-04-02 12:09:46 +0200

    #2 idk2

--- app/controllers/BookController.php
+++ app/controllers/BookController.php
@@ -3,6 +3,6 @@
 class BookController {
     //výchozí metoda pro zobrazení úvodní stránky
     public function index(){
-        require_once '../app/views/books/books_list.php'
+        require_once '../app/controllers/views/books/books_list.php';
     }
 }
